1b3567ede9123666507a11715c61b4abded944f1
kleidione Freitas <kleidione@gmail.com>
2025-03-15 16:18:11 -0300

    Add timestamp-based update filtering to prevent showing current builds
    Implement timestamp comparison to avoid displaying already installed builds
    as available updates. This adds a new isUpdateAvailable() method that checks
    if the remote update timestamp is newer than the current installed build timestamp.
    
    Change-Id: I11783122e8cca33488e43e3fd03df48b21602d91
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -376,6 +376,14 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         }
     };
 
+    private boolean isUpdateAvailable(UpdateInfo update) {
+        long updateTimestamp = update.getTimestamp();
+        long buildTimestamp = BuildInfoUtils.getBuildDateTimestamp();
+        long buildTimestampSeconds = buildTimestamp / 1000;
+    
+        return updateTimestamp > buildTimestampSeconds;
+    }
+
     private void loadUpdatesList(File jsonFile, boolean manualRefresh)
             throws IOException, JSONException {
         Log.d(TAG, "Adding remote updates");
@@ -385,8 +393,10 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         List<UpdateInfo> updates = Utils.parseJson(jsonFile, true);
         List<String> updatesOnline = new ArrayList<>();
         for (UpdateInfo update : updates) {
-            newUpdates |= controller.addUpdate(update);
-            updatesOnline.add(update.getDownloadId());
+            if (isUpdateAvailable(update)) {
+                newUpdates |= controller.addUpdate(update);
+                updatesOnline.add(update.getDownloadId());
+            }
         }
         controller.setUpdatesAvailableOnline(updatesOnline, true);
 
